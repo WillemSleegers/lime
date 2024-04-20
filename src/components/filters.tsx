0110dc02189778592d1629a1ae84ea193502bb60
Willem Sleegers <w.sleegers@me.com>
2024-04-20 16:58:58 +0200

    Update filters.tsx

--- src/components/filters.tsx
+++ src/components/filters.tsx
@@ -108,7 +108,7 @@ export const Filters = (props: FiltersProps) => {
   const [ranOnce, setRanOnce] = useState(false)
   const [disabled, setDisabled] = useState(true)
   const [error, setError] = useState(false)
-  const [webR, setWebR] = useState<WebR>()
+  const [webR, setWebR] = useState<any>()
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -226,7 +226,7 @@ export const Filters = (props: FiltersProps) => {
     initializeR()
 
     form.handleSubmit(onSubmit)
-  }, [])
+  }, [form, setStatus])
 
   useEffect(() => {
     if (status == "Ready") {
@@ -236,7 +236,7 @@ export const Filters = (props: FiltersProps) => {
       setRanOnce(true)
       setDisabled(false)
     }
-  }, [status])
+  }, [status, form, ranOnce])
 
   return (
     <Collapsible
