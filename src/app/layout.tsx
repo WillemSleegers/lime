371928c876b3be59b53a45ca036215f359978685
willemsleegers <w.sleegers@me.com>
2024-06-04 10:36:52 +0200

    Began work on improving the navigation bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,70 +23,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <div className="flex-col md:flex">
           <div className="border-b">
-            <div className="flex items-center p-4">
-              <MainNav>
-                <div className="flex">
-                  <Link
-                    href="/"
-                    className="whitespace-pre-wrap text-xl font-medium transition-colors hover:text-primary sm:whitespace-nowrap"
-                  >
-                    The Meata-Analysis Project
-                  </Link>
-                  <Image
-                    className="ms-2"
-                    alt="meata-analysis logo"
-                    src="/green-leaf-icon.svg"
-                    width={30}
-                    height={30}
-                  />
-                </div>
-
-                <div className="flex flex-wrap items-center gap-4">
-                  <Link
-                    href="/about/"
-                    className="text-base font-medium transition-colors hover:text-primary"
-                  >
-                    About
-                  </Link>
-                  <Link
-                    href="/meta-analysis/"
-                    className="whitespace-nowrap rounded bg-green-700 px-3 py-2 text-base font-medium text-white transition-colors hover:bg-green-800"
-                  >
-                    Meta-analysis
-                  </Link>
-                  <Link
-                    href="/data/"
-                    className="text-base font-medium transition-colors hover:text-primary"
-                  >
-                    Data
-                  </Link>
-                  <Link
-                    href="/FAQ/"
-                    className="text-base font-medium transition-colors hover:text-primary"
-                  >
-                    FAQ
-                  </Link>
-                  <Link
-                    href="/contributors/"
-                    className="text-base font-medium transition-colors hover:text-primary"
-                  >
-                    Contributors
-                  </Link>
-                  <Link
-                    href="/contact/"
-                    className="text-base font-medium transition-colors hover:text-primary"
-                  >
-                    Contact
-                  </Link>
-                  <Link
-                    href="/changelog/"
-                    className="text-base font-medium transition-colors hover:text-primary"
-                  >
-                    Changelog
-                  </Link>
-                </div>
-              </MainNav>
-            </div>
+            <MainNav />
           </div>
         </div>
 
--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -1,16 +1,131 @@
-import { cn } from "@/lib/utils"
+"use client"
 
-export function MainNav({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLElement>) {
+import { cn } from "@/lib/utils"
+import Image from "next/image"
+import Link from "next/link"
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "./ui/navigation-menu"
+import { Button } from "./ui/button"
+import { Menu } from "lucide-react"
+import { useEffect, useLayoutEffect, useState } from "react"
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "./ui/collapsible"
+
+const breakpoint = 1100
+
+export function MainNav({ className }: React.HTMLAttributes<HTMLElement>) {
+  const [isLarge, setIsLarge] = useState(true)
+  const [open, setOpen] = useState(true)
+
+  const onResize = () => {
+    setIsLarge(window.innerWidth > breakpoint)
+  }
+
+  useEffect(() => {
+    isLarge ? setOpen(true) : setOpen(false)
+  }, [isLarge])
+
+  useLayoutEffect(() => {
+    onResize()
+    window.addEventListener("resize", onResize)
+    return () => window.removeEventListener("resize", onResize)
+  }, [])
+
   return (
-    <nav
-      className={cn(
-        "flex w-full flex-col items-center justify-between gap-3 lg:flex-row ",
-        className,
-      )}
-      {...props}
-    ></nav>
+    <Collapsible
+      className={`flex w-full justify-between p-4 ${isLarge ? "flex-row" : "flex-col"}`}
+      open={open}
+      onOpenChange={setOpen}
+    >
+      <div className="flex flex-row justify-between">
+        <div className="flex gap-3">
+          <Link
+            href="/"
+            className="whitespace-pre-wrap text-xl font-medium sm:whitespace-nowrap"
+          >
+            The Meata-Analysis Project
+          </Link>
+          <Image
+            alt="meata-analysis logo"
+            src="/green-leaf-icon.svg"
+            height={0}
+            width={0}
+            style={{ width: "30px", height: "30px" }}
+          />
+        </div>
+        <CollapsibleTrigger
+          asChild
+          className={`${isLarge ? "hidden" : "flex"}`}
+        >
+          <Button variant="ghost" size="sm" className="w-9 p-0">
+            <Menu className="h-4 w-4" />
+            <span className="sr-only">Toggle</span>
+          </Button>
+        </CollapsibleTrigger>
+      </div>
+      <CollapsibleContent>
+        <NavigationMenu>
+          <NavigationMenuList className="flex flex-wrap lg:flex-row">
+            <NavigationMenuItem>
+              <Link href="/about" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  About
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link href="/meta-analysis" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Meta-analysis
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link href="/data" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Data
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link href="/FAQ" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  FAQ
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link href="/contributors" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Contributors
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link href="/contact" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Contact
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link href="/changelog" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Changelog
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </CollapsibleContent>
+    </Collapsible>
   )
 }
