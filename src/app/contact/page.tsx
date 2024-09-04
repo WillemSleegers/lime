4a404b3b231303d8c1d0ad5af249e0a264f89f24
willemsleegers <w.sleegers@me.com>
2024-09-04 17:11:27 +0200

    Added email address

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -12,28 +12,28 @@ const Contact = () => {
       </p>
       <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3">
         <Link
-          href="mailto:someone@example.com?subject=Study to add to the LIME project"
+          href="mailto:contact.project.lime@gmail.com?subject=Study to add to the LIME project"
           className="flex flex-col items-center justify-between rounded-md border-2 border-primary bg-transparent p-4 hover:bg-accent hover:text-accent-foreground"
         >
           <MailPlus />
           Add study
         </Link>
         <Link
-          href="mailto:someone@example.com?subject=Question about the LIME project"
+          href="mailto:contact.project.lime@gmail.com?subject=Question about the LIME project"
           className="flex flex-col items-center justify-between rounded-md border-2 border-primary bg-transparent p-4 hover:bg-accent hover:text-accent-foreground"
         >
           <MailQuestion />
           Question
         </Link>
         <Link
-          href="mailto:someone@example.com?subject=Bug report for the LIME project"
+          href="mailto:contact.project.lime@gmail.com?subject=Bug report for the LIME project"
           className="flex flex-col items-center justify-between rounded-md border-2 border-primary bg-transparent p-4 hover:bg-accent hover:text-accent-foreground"
         >
           <MailWarning />
           Bug
         </Link>
         <Link
-          href="mailto:someone@example.com"
+          href="mailto:contact.project.lime@gmail.com"
           className="flex flex-col items-center justify-between rounded-md border-2 border-primary bg-transparent p-4 hover:bg-accent hover:text-accent-foreground"
         >
           <Mail />
